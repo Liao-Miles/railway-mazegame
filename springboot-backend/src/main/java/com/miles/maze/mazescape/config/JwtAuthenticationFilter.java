88b2c9e531b818a1c922126da7d43e3f70f5d22f
Liao-Miles <datou153759@gmail.com>
2025-09-11 11:00:13 +0800

    09/11 10:54

--- springboot-backend/src/main/java/com/miles/maze/mazescape/config/JwtAuthenticationFilter.java
+++ springboot-backend/src/main/java/com/miles/maze/mazescape/config/JwtAuthenticationFilter.java
@@ -31,12 +31,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     new UsernamePasswordAuthenticationToken(username, null, null);
             authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authentication);
-            filterChain.doFilter(request, response);
-        } else {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
-            response.setContentType("application/json");
-            response.getWriter().write("{\"error\": \"Access token 無效或過期\"}");
         }
+        filterChain.doFilter(request, response);
     }
 
     private String getJwtFromRequest(HttpServletRequest request) {
@@ -47,3 +43,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         return null;
     }
 }
+
